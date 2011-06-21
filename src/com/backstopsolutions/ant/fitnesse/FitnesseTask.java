fc462aa81bb38ef32048f4e3916e388095cc4d3f
George Shakhnazaryan <gshakhnazaryan@backstopsolutions.com>
2011-06-21 22:39:04 +0000

    Initial checkin.
    git-svn-id: https://subversion/svn/trunk/ant-fitnesse@139429 9bad824d-64e8-0310-a639-c248c36e3737

---
+++ src/com/backstopsolutions/ant/fitnesse/FitnesseTask.java
@@ -0,0 +1,74 @@
+package com.backstopsolutions.ant.fitnesse;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.Java;
+import org.apache.tools.ant.types.Environment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FitnesseTask extends Java {
+
+    private List<SuiteFilter> suiteFilters = new ArrayList<SuiteFilter>();
+    private String suiteName;
+    private Integer port;
+    private String slimTableFactory;
+
+    public FitnesseTask() {
+        setClassname("fitnesseMain.FitNesseMain");
+        setFailonerror(true);
+        setFork(true);
+    }
+
+    public void addSuiteFilter(SuiteFilter filter) {
+        suiteFilters.add(filter);
+    }
+
+    public void setSuiteName(String suiteName) {
+        this.suiteName = suiteName;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setSlimTableFactory(String slimTableFactory) {
+        this.slimTableFactory = slimTableFactory;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        if (slimTableFactory != null) {
+            Environment.Variable var = new Environment.Variable();
+            var.setKey("fitnesse.slimTables.SlimTableFactory");
+            var.setValue(slimTableFactory);
+            addSysproperty(var);
+        }
+
+        StringBuilder filterLine = new StringBuilder("FundbutterTestSuite?suite&nohistory&format=text");
+        for (SuiteFilter filter : suiteFilters) {
+            filterLine.append("&suiteFilter=").append(filter.getFilterName());
+        }
+
+        createArg().setLine("-c");
+        createArg().setLine(filterLine.toString());
+        createArg().setLine("-p");
+        createArg().setLine(port.toString());
+        createArg().setLine("-r");
+        createArg().setLine("fitnesse");
+
+        super.execute();
+    }
+
+    public static class SuiteFilter {
+        private String filterName;
+
+        public String getFilterName() {
+            return filterName;
+        }
+
+        public void setFilterName(String filterName) {
+            this.filterName = filterName;
+        }
+    }
+}
